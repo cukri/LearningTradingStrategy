1381b6d75af04137f6bb1e3966becdd024cea96a
cukri <sugier.michal26@gmail.com>
2024-09-25 16:19:30 +0200

    Finalized version of project with portfolio.

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import yfinance as yf
 import pandas_ta
 import warnings
 from sklearn.cluster import KMeans
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import risk_models
+from pypfopt import expected_returns
+import matplotlib.ticker as mtick
 warnings.filterwarnings('ignore')
 
 sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
@@ -109,10 +113,112 @@ data.loc[:, factors] = data.groupby('ticker', group_keys=False)[factors].apply(l
 data = data.drop('adj close', axis = 1)
 data  = data.dropna()
 
+target_rsi_values = [30,45,55,70]
+initial_centroids = np.zeros((len(target_rsi_values), 18))
+initial_centroids[:,6] = target_rsi_values
+
 def get_clusters(df):
     df['cluster'] = KMeans(n_clusters=4,
                            random_state=0,
-                           init='random').fit(df).labels_
+                           init=initial_centroids).fit(df).labels_
     return df
 
 data = data.dropna().groupby('date', group_keys = False).apply(get_clusters)
+
+filtered_df = data[data['cluster']==3].copy()
+
+filtered_df = filtered_df.reset_index(level=1)
+filtered_df.index = filtered_df.index+pd.DateOffset(1)
+filtered_df = filtered_df.reset_index().set_index(['date', 'ticker'])
+
+dates = filtered_df.index.get_level_values('date').unique().tolist()
+
+fixed_dates = {}
+
+for d in dates:
+
+    fixed_dates[d.strftime('%Y-%m-%d')] = filtered_df.xs(d, level=0).index.tolist()
+
+def optimize_weights(prices, lower_bound=0):
+
+    returns = expected_returns.mean_historical_return(prices=prices, frequency=252)
+
+    cov = risk_models.sample_cov(prices=prices, frequency=252)
+
+    ef = EfficientFrontier(expected_returns = returns, cov_matrix=cov, weight_bounds=(lower_bound,.1), solver='SCS')
+
+    weights = ef.max_sharpe()
+
+    return ef.clean_weights()
+
+stocks = data.index.get_level_values('ticker').unique().tolist()
+
+new_df = yf.download(tickers=stocks,
+                     start=data.index.get_level_values('date').unique()[0]-pd.DateOffset(months=12),
+                     end=data.index.get_level_values('date').unique()[-1])
+
+returns_dataframe = np.log(new_df['Adj Close']).diff()
+
+portfolio_df = pd.DataFrame()
+
+for start_date in fixed_dates.keys():
+
+    try:
+        end_date = (pd.to_datetime(start_date) + pd.offsets.MonthEnd(0)).strftime('%Y-%m-%d')
+
+        cols = fixed_dates[start_date]
+
+        optimization_start_date = (pd.to_datetime(start_date)-pd.DateOffset(months=12)).strftime('%Y-%m-%d')
+
+        optimization_end_date = (pd.to_datetime(start_date) - pd.DateOffset(days=1)).strftime('%Y-%m-%d')
+
+        optimization_df = new_df[optimization_start_date:optimization_end_date]['Adj Close'][cols]
+
+        optimization_df = new_df['2016-11-01':'2017-10-30']['Adj Close'][fixed_dates['2017-11-01']]
+
+        weights = optimize_weights(prices=optimization_df,lower_bound=round(1/len(optimization_df.columns)*0.5,3))
+
+        weights = pd.DataFrame(weights, index=pd.Series(0))
+
+        temp_df = returns_dataframe[start_date:end_date]
+
+        temp_df = temp_df.stack().to_frame('return').reset_index(level=0)\
+            .merge(weights.stack().to_frame('weight').reset_index(level=0, drop=True), left_index=True, right_index=True)\
+            .reset_index().set_index(['Date', 'Ticker']).unstack().stack()
+
+        temp_df['weighted_return'] = temp_df['return']*temp_df['weight']
+
+        temp_df = temp_df.groupby(level=0)['weighted_return'].sum().to_frame('Strategy_Return')
+
+        portfolio_df = pd.concat([portfolio_df, temp_df],axis = 0)
+
+    except Exception as e:
+        print(e)
+
+pd.DataFrame([1/len(optimization_df.columns) for i in range(len(optimization_df.columns))],
+             index=optimization_df.columns.tolist(),
+             columns=pd.Series(0))
+
+portfolio_df = portfolio_df.drop_duplicates()
+
+spy = yf.download(tickers='SPY',
+                  start='2015-01-01',
+                  end=dt.date.today())
+
+spy_ret = np.log(spy[['Adj Close']]).diff().dropna().rename({'Adj Close':'SPY Buy&Hold'}, axis = 1)
+
+portfolio_df.index = portfolio_df.index.tz_localize(None)
+spy_ret.index = spy_ret.index.tz_localize(None)
+
+portfolio_df = portfolio_df.merge(spy_ret, left_index=True, right_index=True)
+print(portfolio_df)
+
+plt.style.use('ggplot')
+
+portfolio_cumulative_return = np.exp(np.log1p(portfolio_df).cumsum())-1
+
+portfolio_cumulative_return[:'2023-09-29'].plot(figsize=(16,6))
+plt.title('Unsupervised Learning Trading Strategy Returns Over Time')
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1))
+plt.ylabel('Return')
+plt.show()
